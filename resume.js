2b41a49798c58630a3b0ef8ac364574d597db3e6
KeanuVermeulen <53009641+KeanuVermeulen@users.noreply.github.com>
2019-10-08 16:22:13 +0200

    Add files via upload

---
+++ resume.js
@@ -0,0 +1,88 @@
+// --- Online Resume of Keanu Vermeulen --->
+
+// --- This file contains javascript code for my online resume -->
+
+
+// --- Navbar Link Functionality on Scroll --- 
+
+$(window).scroll(function() {
+  var scrollPos = $(window).scrollTop(),
+      navH     = $('#navbar').height();
+  $('.sections').each(function(i){
+    var offT = $(this).offset().top;
+    if((offT-scrollPos-navH) <= 0) {
+      $('.active').removeClass('active')
+      $('#navbar a').eq(i).addClass('active')
+    }
+  })
+});
+
+// --- Hide and Show Hobbies| Interest on Click
+
+function hide_and_show() {
+  var x = document.getElementById("hobbies-and-interests");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+};
+
+// --- TypeWriter Effect ---
+
+var TxtType = function(el, toRotate, period) {
+        this.toRotate = toRotate;
+        this.el = el;
+        this.loopNum = 0;
+        this.period = parseInt(period, 10) || 2000;
+        this.txt = '';
+        this.tick();
+        this.isDeleting = false;
+    };
+
+    TxtType.prototype.tick = function() {
+        var i = this.loopNum % this.toRotate.length;
+        var fullTxt = this.toRotate[i];
+
+        if (this.isDeleting) {
+        this.txt = fullTxt.substring(0, this.txt.length - 1);
+        } else {
+        this.txt = fullTxt.substring(0, this.txt.length + 1);
+        }
+
+        this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+        var that = this;
+        var delta = 200 - Math.random() * 100;
+
+        if (this.isDeleting) { delta /= 2; }
+
+        if (!this.isDeleting && this.txt === fullTxt) {
+        delta = this.period;
+        this.isDeleting = true;
+        } else if (this.isDeleting && this.txt === '') {
+        this.isDeleting = false;
+        this.loopNum++;
+        delta = 500;
+        }
+
+        setTimeout(function() {
+        that.tick();
+        }, delta);
+    };
+
+    window.onload = function() {
+        var elements = document.getElementsByClassName('typewrite');
+        for (var i=0; i<elements.length; i++) {
+            var toRotate = elements[i].getAttribute('data-type');
+            var period = elements[i].getAttribute('data-period');
+            if (toRotate) {
+              new TxtType(elements[i], JSON.parse(toRotate), period);
+            }
+        }
+        // INJECT CSS
+        var css = document.createElement("style");
+        css.type = "text/css";
+        css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid black}";
+        document.body.appendChild(css);
+    };
